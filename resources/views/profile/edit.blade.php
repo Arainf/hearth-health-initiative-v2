2dd4ade632f9fc2b20f0524eacf259eb08892f93
John Prince <johnprince.dev@gmail.com>
2026-02-09 14:14:52 +0800

    Changed the bg color to bg-[#121212]

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,20 +7,20 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-[#121212] shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-[#121212] shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
             @can('use-ai')
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="p-4 sm:p-8 bg-[#121212] shadow sm:rounded-lg">
                     <div class="max-w-xl">
 
                             @include('profile.partials.ai-settings-form')
@@ -29,7 +29,7 @@
                 </div>
             @endcan
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-[#121212] shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
