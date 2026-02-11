b86c81bf8c5feb3715a43ecb21d031bc28fffb0e
Arainf <adrianrainierf@gmail.com>
2026-02-11 18:36:35 +0800

    Web route initial version

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ use App\Http\Controllers\Pages\ComparePageController;
 use App\Http\Controllers\Pages\DashboardPageController;
 use App\Http\Controllers\Pages\DoctorPageController;
 use App\Http\Controllers\Dump\trashController;
+use App\Http\Controllers\Pages\LoginPageController;
+use App\Http\Controllers\Pages\PageController;
 use App\Http\Controllers\Pages\PatientPageController;
 use App\Http\Controllers\Pages\RecordPageController;
 use App\Http\Controllers\PatientController;
@@ -20,9 +22,12 @@ use App\Http\Controllers\Table\TableController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Pages\UnitPageController;
 
-Route::get('/', function () {
-        return view('auth.login');
-});
+
+$encryption = new trashController();
+
+
+Route::get('/', [LoginPageController::class, 'index']);
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/table/patients/{id}/records', [TableController::class, 'patientsOwnRecord']);
@@ -37,9 +42,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/api/getGeneratedContent/{id}', [ApiController::class, 'getGeneratedContent']);
     Route::get('/export/pdf/{id}', [PdfController::class, 'export']);
 
-    Route::get('/patients/{id}', [PatientController::class, 'show'])->name('patientFiles');
-    Route::get('/patients/{id}/edit', [PatientController::class, 'edit'])->name('patientEdit');
-    Route::put('/patients/{id}', [PatientController::class, 'update']);
+
 
 
     Route::get('/api/patients/{id}/records/{action}', [ApiController::class, 'recordsWithPatient']);
@@ -73,54 +76,74 @@ Route::middleware(['auth', 'can:isAdmin'])->group(function () {
 });
 
 /** NEW ROUTES WITH ENCRYPTED ROUTE */
-
+/**
+DOCTOR PAGE
+ */
 Route::middleware('auth')->group(function () {
-    $core = trashController::encrypt('doctor');
-    Route::get("/{$core}", [DoctorPageController::class, 'index'])->name('doctor');
-    Route::get("/table/{$core}", [DoctorPageController::class, 'table']);
+    Route::get('/page/{token}', [PageController::class, 'page'])->name('page');
+    Route::get('/table/{token}', [PageController::class, 'table'])->name('page');
+
+//    Route::get("/{$core}", [DoctorPageController::class, 'index'])->name('doctor');
+//    Route::get("/table/{$core}", [DoctorPageController::class, 'table']);
 });
-
-Route::middleware('auth')->group(function () {
-    $core = trashController::encrypt('dashboard');
-    Route::get("/{$core}", [DashboardPageController::class, 'index'])->name('dashboard');
-    Route::get("/table/{$core}", [DashboardPageController::class, 'table']);
-});
-
-Route::middleware('auth')->group(function () {
-    $core = trashController::encrypt('account');
+///**
+//DASHBOARD PAGE
+// */
+//Route::middleware('auth')->group(function (){
+//    Route::get('/page/{token}', [DashboardPageController::class, 'index'])->name('dashboard');
+////    Route::get("/{$core}", [DashboardPageController::class, 'index'])->name('dashboard');
+////    Route::get("/table/{$core}", [DashboardPageController::class, 'table']);
+//});
+/**
+ACCOUNT PAGE
+ */
+Route::middleware('auth')->group(function () use ($encryption) {
+    $core = $encryption->encrypt('account');
     Route::get("/{$core}", [AccountPageController::class, 'index'])->name('account');
     Route::get("/table/{$core}", [AccountPageController::class, 'table']);
 
     Route::get("/create/{$core}", [RegisteredUserController::class, 'create'])->name('account.create');
 });
-
-Route::middleware('auth')->group(function () {
-   $core = trashController::encrypt('archive');
+/**
+ARCHIVE PAGE
+ */
+Route::middleware('auth')->group(function () use ($encryption) {
+   $core = $encryption->encrypt('archive');
    Route::get("/{$core}", [ArchivePageController::class, 'index'])->name('archive');
    Route::get("/table/{$core}", [AccountPageController::class, 'table']);
 });
-
-Route::middleware('auth')->group(function () {
-    $core = trashController::encrypt('patient');
-    Route::get("/{$core}", [PatientPageController::class, 'index'])->name('patient');
+/**
+PATIENT PAGE
+ */
+Route::middleware('auth')->group(function () use ($encryption) {
+    $core = $encryption->encrypt('patient');
+//    Route::get("/{$core}", [PatientPageController::class, 'index'])->name('patient');
     Route::get("/table/{$core}", [PatientPageController::class, 'table']);
-});
 
-Route::middleware('auth')->group(function () {
-   $core = trashController::encrypt('compare');
+    Route::get("/{$core}/{id}", [PatientController::class, 'show'])->name('patient.list');
+//    Route::get("/{$core}/{id}/{mode}", [PatientController::class, 'edit'])->name('patient.edit');
+    Route::put("/{$core}/{id}", [PatientController::class, 'update']);
+});
+/**
+COMPARE PAGE
+ */
+Route::middleware('auth')->group(function () use ($encryption) {
+   $core = $encryption->encrypt('compare');
    Route::get("/{$core}", [ComparePageController::class, 'index'])->name('compare');
 
 });
-
-Route::middleware('auth')->group(function () {
-    $core = trashController::encrypt('record');
+/**
+ * RECORD PAGE
+*/
+Route::middleware('auth')->group(function () use ($encryption){
+    $core = $encryption->encrypt('record');
     Route::get("/{$core}", [RecordPageController::class, 'index'])->name('record');
     Route::get("/table/{$core}", [RecordPageController::class, 'table']);
 
 });
 
-Route::middleware('auth')->group(function(){
-    $core = trashController::encrypt('unit');
+Route::middleware('auth')->group(function() use ($encryption){
+    $core = $encryption->encrypt('unit');
     Route::get("/{$core}", [UnitPageController::class, 'index'])->name('unit');
 });
 
